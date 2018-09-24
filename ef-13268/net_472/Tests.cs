ec2cc3e8d0313f7a196b8a7e72a66d6f8208cf78
gojanpaolo <go.janpaolo@gmail.com>
2018-09-24 15:08:18 -0500

    Initial commit

---
+++ ef-13268/net_472/Tests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using Xunit;
+
+namespace net_472
+{
+    public class Tests
+    {
+        [Fact]
+        public void Test()
+        {
+            var options = new DbContextOptionsBuilder<Context>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                .Options;
+
+            using(var ctx = new Context(options))
+                ctx.Database.EnsureCreated();
+        }
+    }
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context> options) : base(options) { }
+    }
+}
